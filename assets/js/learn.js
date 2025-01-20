c13faad3cc5328ff57329602b092988cba877c2d
Elkana-DAT <147919326+Elkana-DAT@users.noreply.github.com>
2025-01-20 07:30:08 -0700

    Update learn.js

--- assets/js/learn.js
+++ assets/js/learn.js
@@ -3,7 +3,7 @@ var typingEffect = new Typed(".top",{
     strings : ["Welcome","- let them guide you through the lessons they have taught me, so you can learn faster with purpose.",],
     loop : true,
     typeSpeed : 100, 
-    backSpeed : 90,
-    backDelay : 2000
+    backSpeed : 10,
+    backDelay : 3000
   })
   
