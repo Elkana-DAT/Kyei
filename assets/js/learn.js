db4eda310b1e98fb6455e17fad370c8bf7f1d945
Elkana-DAT <147919326+Elkana-DAT@users.noreply.github.com>
2025-01-20 07:08:35 -0700

    Create learn.js

---
+++ assets/js/learn.js
@@ -0,0 +1,9 @@
+/* ----- TYPING EFFECT ----- */
+var typingEffect = new Typed(".typedText",{
+    strings : ["Welcome","Learn More!!","Earn More!!",],
+    loop : true,
+    typeSpeed : 100, 
+    backSpeed : 90,
+    backDelay : 2000
+  })
+  
